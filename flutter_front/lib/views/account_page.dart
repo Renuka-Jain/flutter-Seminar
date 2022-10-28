97fa8ce4dcd3e9007f2b3404cea01a10a7e748b0
irenegordun <irene.gordun@estudiantat.upc.edu>
2022-10-28 13:10:00 +0200

    form register v1

--- flutter_front/lib/views/account_page.dart
+++ flutter_front/lib/views/account_page.dart
@@ -11,12 +11,55 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPage extends State<AccountPage> {
+  late String name;
+  late String password;
+  late String mail;
+
+  GlobalKey formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Seminari 10 Fluter ACCOUNT'),
+        title: const Text('Seminari 10 Fluter REGISTER'),
         backgroundColor: Colors.deepPurple[300], 
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: "Name"),
+                onSaved: (value){
+                  name = value!;
+                },
+              ),
+              TextFormField(
+                decoration : InputDecoration (labelText: "password"),
+                onSaved: (value){
+                 password = value!;
+                },
+              ),
+              TextFormField(
+                decoration : InputDecoration (labelText: "mail"),
+                onSaved: (value){
+                  mail = value!;
+                },
+              ),
+            FloatingActionButton.small(
+              onPressed: () {
+                //funció per registrar
+              },
+              backgroundColor: Color.fromARGB(255, 9, 51, 85),
+              tooltip: 'Send',
+              child: const Icon (Icons.navigation),
+            ),
+            ],
+            //FloatingActionButtonLocation.endFloat,
+          ),
+        ),
       ),
   );
 
